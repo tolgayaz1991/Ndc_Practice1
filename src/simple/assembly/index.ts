6db550e5d99ac91fe686f9c7ea054b9a113a484a
Tolga Yaz <tolgayaz1991@gmail.com>
2022-04-04 19:33:22 +0300

    Task3

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,8 +1,8 @@
 import { storage, Context } from "near-sdk-as"
 
-// return the string 'hello <name>'
-export function helloWorld(name: string): string {
-  return 'hello ' + name
+// return the string 'hello <namearray>'
+export function helloWorld(names: Array<string>): string {
+  return names.map<string>(name => 'hello ' + name).join(` `)
 }
 
 // read the given key from account (contract) storage
